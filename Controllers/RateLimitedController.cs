d1691bcd39c71ad62316ec3e33b8487d79c01797
Fernando Pinto <fnandop@gmail.com>
2024-07-28 22:00:20 +0100

    wait until poc

--- Controllers/RateLimitedController.cs
+++ Controllers/RateLimitedController.cs
@@ -24,12 +24,24 @@ namespace SlidingWindowRateLimiter.Controllers
         public async Task<IActionResult> Sliding([FromHeader] string authorization)
         {
             var encoded = string.Empty;
-            if(!string.IsNullOrEmpty(authorization)) encoded = AuthenticationHeaderValue.Parse(authorization).Parameter;
-            if (string.IsNullOrEmpty(encoded)) return new UnauthorizedResult();
-            var apiKey = Encoding.UTF8.GetString(Convert.FromBase64String(encoded)).Split(':')[0];
-            var limited = ((int) await _db.ScriptEvaluateAsync(Scripts.SlidingRateLimiterScript,
-                new {key = new RedisKey($"{Request.Path}:{apiKey}"), window = 30, max_requests = 10})) == 1;
+
+            var apiKey = "batata";// Encoding.UTF8.GetString(Convert.FromBase64String(encoded)).Split(':')[0];
+            var limited = ((int)await _db.ScriptEvaluateAsync(Scripts.SlidingRateLimiterScript,
+                new { key = new RedisKey($"{Request.Path}:{apiKey}"), window = 1, max_requests = 10 })) == 1;
             return limited ? new StatusCodeResult(429) : Ok();
+        }
+
+        [HttpPost]
+        [HttpGet]
+        [Route("waituntil")]
+        public async Task<IActionResult> WaitUntil()
+        {
+            var encoded = string.Empty;
+
+
+            var waitInMs = ((int)await _db.ScriptEvaluateAsync(Scripts.SlidingRateLimiterWaitUntilScript));
+            await Task.Delay(waitInMs);
+            return  Ok();
         }
     }
 }
--- Scripts.cs
+++ Scripts.cs
@@ -7,16 +7,34 @@ namespace SlidingWindowRateLimiter
         public static LuaScript SlidingRateLimiterScript => LuaScript.Prepare(SlidingRateLimiter);
         private const string SlidingRateLimiter = @"
             local current_time = redis.call('TIME')
-            local trim_time = tonumber(current_time[1]) - @window
+            local current_time_ms = current_time[1]*1000 + current_time[2]/1000
+            local window_ms = @window*1000
+            local trim_time = tonumber(current_time_ms) - window_ms
             redis.call('ZREMRANGEBYSCORE', @key, 0, trim_time)
             local request_count = redis.call('ZCARD',@key)
 
             if request_count < tonumber(@max_requests) then
-                redis.call('ZADD', @key, current_time[1], current_time[1] .. current_time[2])
+                redis.call('ZADD', @key, current_time_ms, current_time[1] .. current_time[2])
                 redis.call('EXPIRE', @key, @window)
                 return 0
             end
             return 1
             ";
+
+        public static LuaScript SlidingRateLimiterWaitUntilScript => LuaScript.Prepare(SlidingRateLimiterWaitUntil);
+        private const string SlidingRateLimiterWaitUntil = @"
+            local current_time = redis.call('TIME')
+            local current_time_ms = current_time[1]*1000 + current_time[2]/1000
+            local unit_wait_time_ms = 100
+            local current_wait_until_ms  = redis.call('GET', 'CURRENT_WAIT_UNTIL_MS')
+            if not current_wait_until_ms then
+              current_wait_until_ms = 0
+            end
+            current_wait_until_ms = math.max(current_time_ms,current_wait_until_ms + unit_wait_time_ms )
+
+            redis.call('SET', 'CURRENT_WAIT_UNTIL_MS',current_wait_until_ms)
+            redis.call('EXPIRE', 'CURRENT_WAIT_UNTIL_MS', 1)
+            return current_wait_until_ms - current_time_ms
+             ";
     }
 }
